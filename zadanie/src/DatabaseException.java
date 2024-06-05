5ec44d8599c7a90491335c007d9a0fe0cf207db4
Roman <166123062+lorumo@users.noreply.github.com>
2024-06-05 10:22:34 +0200

    Delete zadanie directory

--- zadanie/src/DatabaseException.java
+++
@@ -1,6 +0,0 @@
-public class DatabaseException extends RuntimeException {
-    // if the  same user is added to DB code throws this error
-    public DatabaseException(String error) {
-        super(error);
-    }
-}
--- zadanie/src/Payer.java
+++
@@ -1,3 +0,0 @@
-interface Payer {
-    float getPrice();
-}
